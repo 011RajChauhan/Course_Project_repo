923c7df9222c085f104618fcaba1f98d3f47671a
Rajan Chauhan <Rajan Chauhan@DELL>
2016-09-06 22:04:20 +0530

    session and servlet request

--- Course-Project/src/main/java/com/spring/controllers/CourseController.java
+++ Course-Project/src/main/java/com/spring/controllers/CourseController.java
@@ -1,5 +1,8 @@
 package com.spring.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,42 +21,52 @@ public class CourseController {
 	
 	@RequestMapping(value="{projectId}")
 	public String find(Model model,@PathVariable("projectId") int projectId){
+		System.out.println("invoking /project/{projectId} >>> find()");
 		model.addAttribute("project",this.projectServices.find(projectId));
 		return "project";
 	}
+	
+	/*same URL mapping name used for different type of 
+	action to be executed according to the parameter 
+	obtained from the form*/
+	
 	@RequestMapping(value="/add",method=RequestMethod.GET)
-	public String addProject(){
-		System.out.println("addProject");
+	public String addProject(HttpSession session){
+		session.setAttribute("user","rajan");
+		System.out.println("invoking addProject()");
 		return "project_add";
 	}
 	
 	@RequestMapping(value="/add",method=RequestMethod.POST)
-	public String saveProject(){
-		System.out.println("saveProject");
+	public String saveProject(HttpServletRequest request,HttpSession session){
+		System.out.println("invoking saveProject()");
+		System.out.println(request.getParameter("name"));
+		System.out.println(session.getAttribute("user"));
 		return "project_add";
 	}
 	
 	@RequestMapping(value="/add",method=RequestMethod.POST,params={"type=mutli"})
 	public String saveMultiYearProject(){
-		System.out.println("saveMultiYearProject");
+		System.out.println("invoking saveMultiYearProject()");
 		return "project_add";
 	}
 	
 	@RequestMapping(value="/add",method=RequestMethod.POST,params={"type=mutli","special"})
 	public String saveSpecialProject(){
-		System.out.println("saveSpecialProject");
+		System.out.println("invoking saveSpecialProject()");
 		return "project_add";
 	}
 	
 	@RequestMapping(value="/add",method=RequestMethod.POST,params={"special"})
 	public String saveSpecial2Project(){
-		System.out.println("saveSpecial2Project");
+		System.out.println("invoking saveSpecial2Project()");
 		return "project_add";
 	}
 	
 	
 	@RequestMapping(value="/find",method=RequestMethod.GET)
 	public String find(Model model){
+		System.out.println("invoking find() for all projects");
 		model.addAttribute("projects",projectServices.findAll());
 		return "projects";
 	}
